1f5267e8fae93b0936d3a066dadef420b1539e0b
Jesstern Rays <jsstrn@users.noreply.github.com>
2019-03-12 14:12:00 +0800

    Remove console.log because we knows the debugs, yo!

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const port = 8080;
 const server = http.createServer();
 
 server.on("request", (req, res) => {
-  // respond to requests
-  console.log("Received request", req);
-  console.log("route", req.url);
-
   if (req.url === "/watermelon") {
     res.end("This is a watermelon....");
   } else {
